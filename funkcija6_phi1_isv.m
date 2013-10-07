760dfc155b8abb2dd5deb7ce188cb6c370f70a03
kristinaz <k.ziedaainyte@gmail.com>
2013-10-07 12:02:09 +0300

    papildymas phi1 ir phi2 funkciju isvestiniu

---
+++ funkcija6_phi1_isv.m
@@ -0,0 +1,5 @@
+function pirmas_1 = funkcija6_phi1_isv(x)
+%pirmoji funkcija (x)^2-2
+%cia skaiciuosime pirmos funkcijos isvesvestine
+pirmas_1=2x;
+end
---
+++ funkcija6_phi2_isv.m
@@ -0,0 +1,8 @@
+function antras_2 = funkcija6_phi2_isv(x)
+%antra funkcija sqrt(x+2)
+%skaiciuojame antros funkcijos isvestine
+if x < 2
+    error('x>2!');
+end
+antras_2=(1/(2*sqrt(x+2)));
+end
